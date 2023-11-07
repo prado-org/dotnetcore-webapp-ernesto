4b4b3e06ae47ec000faa11e590f6a523bb210514
Leandro Prado <leadro@microsoft.com>
2023-11-07 09:21:49 -0300

    add project

---
+++ src/MyFirstProject.Tests/UnitTest2.cs
@@ -0,0 +1,24 @@
+namespace MyFirstProject.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
