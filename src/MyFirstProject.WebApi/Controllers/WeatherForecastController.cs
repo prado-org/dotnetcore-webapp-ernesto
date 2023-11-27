3639a2affa08e55af85f7dc75fd7aea9d51b3159
Leandro Prado <69152421+leandromsft@users.noreply.github.com>
2023-11-27 13:51:10 -0300

    Update WeatherForecastController.cs

--- src/MyFirstProject.WebApi/Controllers/WeatherForecastController.cs
+++ src/MyFirstProject.WebApi/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace MyFirstProject.WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -28,6 +28,36 @@ namespace MyFirstProject.WebApi.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        private bool GetEmployeePostgreSql(int id)
+        {
+            try
+            {
+                string conexao = "User ID=leandro;Password=testeLeandroPrado;Host=postgresqlserver-platinst02sandbox.postgres.database.azure.com;Port=5432;Database=Employee;Pooling=true;";
+                
+                using NpgsqlConnection connection = new NpgsqlConnection(conexao);
+                connection.OpenAsync();
+                
+                string selectCommand = "SELECT * FROM Employee WHERE id = " + id.ToString();
+
+                NpgsqlCommand command = new NpgsqlCommand(selectCommand, connection);
+
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int key = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+                    }
+                }
+
+                return true;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
     }
 }
