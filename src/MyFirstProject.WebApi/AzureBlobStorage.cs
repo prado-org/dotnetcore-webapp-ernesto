543b62bd207046c10a5798b36730fbdc1af81b0f
Leandro Prado <leadro@microsoft.com>
2023-11-27 14:45:53 -0300

    tests advaned security

---
+++ src/MyFirstProject.WebApi/AzureBlobStorage.cs
@@ -0,0 +1,32 @@
+using Azure.Storage.Blobs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.WebApi
+{
+    public class AzureBlobStorage
+    {
+        private BlobContainerClient _containerClient;
+
+        public AzureBlobStorage()
+        {
+            string connectionString = "+LXpHumptdfyu/45QHCIPT1YsbyAoMNAwI0lIgjPFuJEl2LyflvnIvLHbjJxb73X7x7thzAcdyHE+AStjy9ClA==";
+            string containerName = "myfirstproject";
+            _containerClient = new BlobContainerClient(connectionString, containerName);
+        }
+
+        public async Task<IEnumerable<string>> ListBlobsAsync()
+        {
+            var blobList = new List<string>();
+            await foreach (var blobItem in _containerClient.GetBlobsAsync())
+            {
+                blobList.Add(blobItem.Name);
+            }
+            return blobList;
+        }
+
+
+        
+        
+    }
+}
