1d262ce3d73206a7436496907c6f2dcfd9630659
Leandro DEV01 <95709260+leandro-dev01@users.noreply.github.com>
2023-11-28 09:43:25 -0300

    Update ErrorViewModel.cs

--- src/MyFirstProject.WebApp/Models/ErrorViewModel.cs
+++ src/MyFirstProject.WebApp/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace MyFirstProject.WebApp.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
