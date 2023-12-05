fb947e45678f535e5f36e66be3424ff5d15d698d
Leandro Prado <leadro@microsoft.com>
2023-12-05 13:35:06 -0300

    remove comment

--- src/MyFirstProject.WebApi/Controllers/WeatherForecastController.cs
+++ src/MyFirstProject.WebApi/Controllers/WeatherForecastController.cs
@@ -22,7 +22,6 @@ namespace MyFirstProject.WebApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            //outro comentário do treianmento
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
